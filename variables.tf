6b4d3c0377b470de622e6ce790967fd7af6944e9
himanshu-uniyal <himanshu@clouddrove.com>
2020-10-27 17:31:08 +0530

    updated and upgraded

--- variables.tf
+++ variables.tf
@@ -62,7 +62,7 @@ variable "enabled" {
 
 variable "max_session_duration" {
   type        = string
-  default     = ""
+  default     = "3600"
   description = " - (Optional) The maximum session duration (in seconds) that you want to set for the specified role. If you do not specify a value for this setting, the default maximum of one hour is applied. This setting can have a value from 1 hour to 12 hours."
 }
 
